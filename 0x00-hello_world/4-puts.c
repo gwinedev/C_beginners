fa7b9ef50be4b2b0f2baf4be29fbb2d024d52f65
Okoeguale Godwin <okoegualegodwin@gmail.com>
2025-02-21 21:38:01 +0100

    work on file export to other file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    puts("\"Programming is like building a multilingual puzzle");
+    puts("\n");
+    return 0;
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Size of char: %ld byte(s)\n", sizeof(char));
+    printf("Size of int: %ld byte(s)\n", sizeof(int));
+    printf("Size of long: %ld byte(s)\n", sizeof(long));
+    printf("Size of long long int: %ld byte(s)\n", sizeof(long long int));
+    return (0);
+}
