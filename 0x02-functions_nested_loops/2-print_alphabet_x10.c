1f7c72a1f639950b4260074233f2f18d42fe9939
Okoeguale Godwin <okoegualegodwin@gmail.com>
2025-02-24 15:59:49 +0100

    still working on nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+void print_alphabet_x10(void) {
+    int num;
+    char letter;
+
+    for(num = 0; num < 10; num++) {
+        letter = 'a';
+        while(letter <= 'z')
+        {
+            _putchar(letter);
+            letter++;
+        }
+        _putchar('\n');
+    }
+
+    
+}
