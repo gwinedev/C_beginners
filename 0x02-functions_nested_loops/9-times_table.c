2ae6c1e3b58cf39109fca9614b7dcb373a6043ab
Okoeguale Godwin <okoegualegodwin@gmail.com>
2025-02-26 21:10:18 +0100

    edit file 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,21 +1,25 @@
 #include "main.h"
 
 void times_table(void) {
-    int a, b, c;
+    int  a, b, c;
 
     for (a = 0; a < 10; a++) {
         for (b = 0; b < 10; b++) {
             c = a * b;
-            if (c < 10) {
+            if (b == 0) {
                 _putchar('0' + c);
-                _putchar(',');
-                _putchar(' ');
             }
-            else {
-                _putchar((c / 10) + '0');
-                _putchar((c % 10) + '0');
+            if (c < 10 && b != 0) {
                 _putchar(',');
                 _putchar(' ');
+                _putchar(' ');
+                _putchar('0' + c);
+            }
+            else if (c >= 10) {
+                _putchar(',');
+                _putchar(' ');
+                _putchar('0' + (c / 10));
+                _putchar('0' + (c % 10));
             }
         }
         _putchar('\n');
