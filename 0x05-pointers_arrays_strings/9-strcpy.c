e6cf59a317ca576753576eb5bacada054526f864
Okoeguale Godwin <okoegualegodwin@gmail.com>
2025-03-07 21:56:48 +0100

    add file for strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+char *_strcpy(char *dest, char *src) {
+    int length = 0, i;
+
+    while (src[length] != '\0')
+        length++;
+
+    for (i = 0; i < length; i++) {
+        dest[i] = src[i];
+    }
+    dest[length] = '\0';
+    return(dest);
+}
